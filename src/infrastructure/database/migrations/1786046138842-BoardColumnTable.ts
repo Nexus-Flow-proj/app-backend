dbe6345eb2b008c57a1790e25b524d7ebbab6ba5
Khaled Khedr <khaledkhedr18@gmail.com>
2026-08-06 23:20:19 +0300

    fix(db): fixed the database migration that caused the database to crash

--- src/infrastructure/database/migrations/1786046138842-BoardColumnTable.ts
+++ src/infrastructure/database/migrations/1786046138842-BoardColumnTable.ts
@@ -7,39 +7,30 @@ export class BoardColumnTable1786046138842 implements MigrationInterface {
     await queryRunner.query(
       `DROP INDEX "public"."IDX_082a77641034b050a2ff5a0970"`,
     );
-    await queryRunner.query(`ALTER TABLE "tasks" ADD "assigned_by_id" uuid`);
-    await queryRunner.query(`ALTER TABLE "board_columns" DROP COLUMN "name"`);
     await queryRunner.query(
-      `ALTER TABLE "board_columns" ADD "name" character varying(100) NOT NULL`,
-    );
-    await queryRunner.query(`ALTER TABLE "board_columns" DROP COLUMN "color"`);
-    await queryRunner.query(
-      `ALTER TABLE "board_columns" ADD "color" character varying(20) NOT NULL`,
+      `ALTER TABLE "board_columns" ALTER COLUMN "name" TYPE character varying(100)`,
     );
     await queryRunner.query(
-      `CREATE INDEX "IDX_5b52433cc346d8e1d8c6d73727" ON "subtasks"  ("task_id", "sort_order", "created_at") `,
+      `ALTER TABLE "board_columns" ALTER COLUMN "color" TYPE character varying(20)`,
     );
     await queryRunner.query(
-      `CREATE INDEX "IDX_8d82387ba026be63046895fe37" ON "tasks"  ("project_id", "status") `,
+      `CREATE INDEX "IDX_5b52433cc346d8e1d8c6d73727" ON "subtasks" ("task_id", "sort_order", "created_at")`,
     );
     await queryRunner.query(
-      `CREATE INDEX "IDX_39d8e89d36488a8e76315792f0" ON "tasks"  ("board_column_id", "column_order") `,
+      `CREATE INDEX "IDX_8d82387ba026be63046895fe37" ON "tasks" ("project_id", "status")`,
     );
     await queryRunner.query(
-      `CREATE INDEX "IDX_a5ea01d2307b59a82b8263a9cf" ON "board_columns"  ("project_id", "name") `,
+      `CREATE INDEX "IDX_39d8e89d36488a8e76315792f0" ON "tasks" ("board_column_id", "column_order")`,
     );
     await queryRunner.query(
-      `CREATE INDEX "IDX_933976b397251806fa5114eed9" ON "board_columns"  ("project_id", "sort_order") `,
+      `CREATE INDEX "IDX_a5ea01d2307b59a82b8263a9cf" ON "board_columns" ("project_id", "name")`,
     );
     await queryRunner.query(
-      `ALTER TABLE "tasks" ADD CONSTRAINT "FK_3e08a7ca125a175cf899b09f71a" FOREIGN KEY ("assigned_by_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE NO ACTION`,
+      `CREATE INDEX "IDX_933976b397251806fa5114eed9" ON "board_columns" ("project_id", "sort_order")`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "tasks" DROP CONSTRAINT "FK_3e08a7ca125a175cf899b09f71a"`,
-    );
     await queryRunner.query(
       `DROP INDEX "public"."IDX_933976b397251806fa5114eed9"`,
     );
@@ -55,17 +46,14 @@ export class BoardColumnTable1786046138842 implements MigrationInterface {
     await queryRunner.query(
       `DROP INDEX "public"."IDX_5b52433cc346d8e1d8c6d73727"`,
     );
-    await queryRunner.query(`ALTER TABLE "board_columns" DROP COLUMN "color"`);
     await queryRunner.query(
-      `ALTER TABLE "board_columns" ADD "color" character varying NOT NULL`,
+      `ALTER TABLE "board_columns" ALTER COLUMN "color" TYPE character varying`,
     );
-    await queryRunner.query(`ALTER TABLE "board_columns" DROP COLUMN "name"`);
     await queryRunner.query(
-      `ALTER TABLE "board_columns" ADD "name" character varying NOT NULL`,
+      `ALTER TABLE "board_columns" ALTER COLUMN "name" TYPE character varying`,
     );
-    await queryRunner.query(`ALTER TABLE "tasks" DROP COLUMN "assigned_by_id"`);
     await queryRunner.query(
-      `CREATE INDEX "IDX_082a77641034b050a2ff5a0970" ON "subtasks" USING btree ("id", "task_id") `,
+      `CREATE INDEX "IDX_082a77641034b050a2ff5a0970" ON "subtasks" USING btree ("id", "task_id")`,
     );
   }
 }
